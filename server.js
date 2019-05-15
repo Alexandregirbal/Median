280e91d08638f6b87dcb81e82f2f3f686d1f924a
Alexandre <alexandre.girbal.pro@gmail.com>
2019-05-15 05:53:04 +0200

    d-d-1.2

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ var bodyParser = require("body-parser")
 var app = express()
 var port = process.env.PORT || 3000
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static(__dirname + '/dist'));
 
 
-var Users = require("./routes/Users")
-var Marks = require("./routes/Marks")
-var Subjects = require("./routes/Subjects")
+var Users = require("./routes/Users");
+var Marks = require("./routes/Marks");
+var Subjects = require("./routes/Subjects");
 
 
-app.use("/api", Users)
-app.use("/api", Marks)
-app.use("/api", Subjects)
+app.use("/api", Users);
+app.use("/api", Marks);
+app.use("/api", Subjects);
 
 
 app.listen(port, function(){
